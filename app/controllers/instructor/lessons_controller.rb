da4f5125ee056ad36b98f3ead6b972c51a215c12
Stephen Macias <stephenmacias@yahoo.com>
2019-07-27 03:06:35 +0000

    more permissions and refactoring courses/sections/lessons

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,24 +1,23 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
 
   def new
-    @section = current_section
-    if @section.course.user != current_user
-      return render plain: 'Unauthorized', status: :Unauthorized
-    end
     @lesson = Lesson.new
   end
 
   def create
-    @section = current_section
-    if @section.course.user != current_user
-      return render plain: 'Unauthorized', status: :Unauthorized
-    end
-    @lesson = @section.lessons.create(lesson_params)
-    redirect_to instructor_course_path(@section.course)
+    @lesson = current_section.lessons.create(lesson_params)
+    redirect_to instructor_course_path(current_section.course)
   end
 
   private
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      return render plain: 'Unauthorized', status: :Unauthorized
+    end
+  end
 
   helper_method :current_section
   def current_section
