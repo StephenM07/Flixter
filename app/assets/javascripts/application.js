dbfbd639d67c790beb3ca603f05f356bd9193e52
Stephen Macias <stephenmacias@yahoo.com>
2019-08-12 18:11:44 +0000

    changed pg image from .png to .jpg

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,10 @@
 //= require rails-ujs
 //= require activestorage
 //= require jquery
-//= require jquery_ujs
 //= require turbolinks
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
 //= require_tree .
 //= require jquery-ui
+//= require jquery_ujs
